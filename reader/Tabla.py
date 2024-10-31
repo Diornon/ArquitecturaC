9cbc015c0e1b1a31d53abb58da836d826bcecf86
Diornon <132793602+Diornon@users.noreply.github.com>
2024-10-31 09:40:29 -0500

    Actualizar Tabla.py

--- reader/Tabla.py
+++ reader/Tabla.py
@@ -1,22 +1,15 @@
-from collections import Counter
-from operator import indexOf
 
 import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.pyplot import savefig
-from numexpr.necompiler import double
+
 
 with open('pings', 'r') as file:
     numbers = [line.split("=") for line in file]
 nums = []
 numbers.pop(0)
 for i in numbers:
-    print(i[-1].split(" ")[0].strip())
-
     a = double(i[-1].split(" ")[0].strip())
 
     nums.append(a)
-print(nums.reverse())
 
 
 
